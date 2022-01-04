140cb19d8efd40b8248833f492d52533bcd4d08e
pabloescobar007 <ngrahul1@gmail.com>
2022-01-04 21:42:38 +0800

    threads

--- tg_bot.py
+++ tg_bot.py
@@ -323,5 +323,5 @@ def hello():
     
 if __name__ == '__main__':
     port = int(environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True,Threaded=True)
+    app.run(host="0.0.0.0", port=port, debug=True,threaded=True)
     #main()
